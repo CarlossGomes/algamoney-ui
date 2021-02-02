5079d48cb6c3e6e1c707ee3d40b6f2b91726d642
CarlossGomes <carlosdaniel_ag@hotmail.com>
2021-02-02 16:43:27 -0300

    Tabela de dados

---
+++ src/app/Lancamento.ts
@@ -0,0 +1,8 @@
+export class Lancamento {
+  public tipo: string;
+  public descricao: string;
+  public dataVencimento: string;
+  public dataPagamento?: string;
+  public valor: number;
+  public pessoa: string;
+}
