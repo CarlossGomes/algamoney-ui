9b50d81fce730aed37b5e8f64276a9064ae582ab
CarlossGomes <carlosdaniel_ag@hotmail.com>
2021-04-14 10:08:54 -0300

    paginação

--- src/app/lancamento/pages/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamento/pages/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -1,5 +1,6 @@
 import { LancamentoFiltro, LancamentoService } from './../../services/lancamento.service';
 import { Component, OnInit } from '@angular/core';
+import { LazyLoadEvent } from 'primeng/api';
 
 @Component({
   selector: 'app-lancamentos-pesquisa',
@@ -8,22 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LancamentosPesquisaComponent implements OnInit {
 
+  totalRegistros = 0;
   filtro = new LancamentoFiltro();
   public lancamentos = [];
 
   constructor(private lancamentoService: LancamentoService) { }
 
   ngOnInit(): void {
-    this.pesquisar();
+    // this.pesquisar();
   }
 
-  public pesquisar(): void {
+  public pesquisar(pagina = 0): void {
+    this.filtro.pagina = pagina;
     this.lancamentoService.pesquisar(this.filtro).subscribe(
       response => {
         this.lancamentos = response.content;
-        console.log(response.totalElements);
+        this.totalRegistros = response.totalElements;
       }
-    )
+    );
+  }
+
+  public aoMudarPagina(event: LazyLoadEvent) {
+    const pagina = event.first / event.rows;
+    this.pesquisar(pagina);
   }
 
 }
