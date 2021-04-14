d3e75f39639e8bc04ab54d3d9b15be77df6f6cb4
CarlossGomes <carlosdaniel_ag@hotmail.com>
2021-04-14 16:33:41 -0300

    pessoas dropdown

--- src/app/lancamento/pages/lancamento-cadastro/lancamento-cadastro.component.ts
+++ src/app/lancamento/pages/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -1,3 +1,4 @@
+import { PessoaService } from './../../../pessoa/services/pessoa.service';
 import { ErrorHandlerService } from 'src/app/core/error-handler.service';
 import { CategoriaService } from './../../../categoria/services/categoria.service';
 import { Component, OnInit } from '@angular/core';
@@ -19,10 +20,12 @@ export class LancamentoCadastroComponent implements OnInit {
   pessoas = [];
 
   constructor(private categoriaService: CategoriaService,
-    private errorHandler: ErrorHandlerService) { }
+              private pessoaService: PessoaService,
+              private errorHandler: ErrorHandlerService) { }
 
   ngOnInit(): void {
     this.carregarCategorias();
+    this.carregarPessoas();
   }
 
   public carregarCategorias(): object {
@@ -32,7 +35,18 @@ export class LancamentoCadastroComponent implements OnInit {
       }, erro => {
         this.errorHandler.handle(erro);
       }
-    )
+    );
+  }
+
+  public carregarPessoas(): object {
+    return this.pessoaService.listarTodas().subscribe(
+      response => {
+        this.pessoas = response.content.map(p => ({ label: p.nome, value: p.codigo }));
+        console.log(this.pessoas)
+      }, erro => {
+        this.errorHandler.handle(erro);
+      }
+    );
   }
 
 }
